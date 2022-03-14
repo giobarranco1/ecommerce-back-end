afa48d4a8ff90a2722ad25c2aecaab40be01ec4d
gio_barranco <sandiego_94@hotmail.com>
2022-03-13 18:41:12 -0600

    set up the fields and rules in tag  model

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,19 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      //Doesnt allow null values
+      allowNull: false,
+      //set as primary key
+      primaryKey: true,
+      //Uses auto increment 
+      autoIncrement: true,
+    },
+    tag_name: {
+      //String
+      type: DataTypes.STRING,
+    }
   },
   {
     sequelize,
