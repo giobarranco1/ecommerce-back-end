d870563a1efaa125bf4e12e4845d01af086e6cbd
gio_barranco <sandiego_94@hotmail.com>
2022-03-13 19:32:55 -0600

    updated the category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,48 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  const allCategories = await Category.findAll({
+    // be sure to include its associated Products
+    include: [{model: Product}]
+  })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  const singleCategory = await Category .findByPk(req.params.id, {
+    // be sure to include its associated Products
+    include: [{model:Product}]
+  })
+  res.status(200).json(singleCategory);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  const newCategory = await Category.create({
+    //uses body category_name: new category in insomnia 
+    category_name: req.body.category_name
+  })
+  res.status(200).json(newCategory);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  const updateCategory = await Category.update(
+    // use body "category_name": "new Category name" to update target category in insomnia 
+    {category_name: req.body.category_name},
+    // use the id in the url to determine which category gets updated
+    {where: {id: req.params.id}}
+  );
+  res.status(200).json(updateCategory)
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  const deleteCategory = await Category.destroy({
+    where: {id: req.params.id}
+  });
 });
 
 module.exports = router;
