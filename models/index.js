87cc7484d1014c3532c661e06706126bd263acfa
gio_barranco <sandiego_94@hotmail.com>
2022-03-13 23:43:42 -0600

    updated the index models

--- models/index.js
+++ models/index.js
@@ -1,22 +1,34 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey:"category_id"
-})
+  foreignKey: "category_id",
+});
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: "category_id"
+  foreignKey: "category_id",
 });
 
 //need to ask office hours/tutor
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // Product is tied to tag through Product tag so
+  // we need to indicate that with "through: ProductTag"
+  through: ProductTag,
+  foreignKey: "product_id",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // Same as above
+  // connected to product through ProductTag
+  through: ProductTag,
+  foreignKey:"tag_id"
+})
 
 module.exports = {
   Product,
